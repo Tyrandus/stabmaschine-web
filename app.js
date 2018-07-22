cf99e18d7f8769b268e8352700477a6bcf6c991d
Florian Knoch <florian.knoch@stud.uni-bamberg.de>
2018-07-22 23:12:12 +0200

    Add more keyboard shortcuts

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ window.onload = function () {
   const v = new Vue({
     el: '#app',
     data: {
+      isManualActive: false,
       activeRodIndex: 0,
       rods: [
         { sliceCount: 2 },
         { sliceCount: 5 },
         { sliceCount: 3 },
+        { sliceCount: 0 },
         { sliceCount: 0 },
         { sliceCount: 0 },
         { sliceCount: 0 }
@@ -27,14 +29,16 @@ window.onload = function () {
       addRod: function () {
         this.rods.push({ sliceCount: 0 })
       },
-      removeRod: function () {
-        if (this.rods.length > 0) {
-          this.rods.pop()
+      removeRod: function (index) {
+        if (index < this.rods.length) {
+          this.rods.splice(index, 1)
+          if (this.activeRodIndex === this.rods.length) {
+            this.activeRodIndex--
+          }
         }
       },
       mousePressed: function (event) {
         // TODO: Check for consistency throughout the OSs
-        // TODO: Add shortcuts for adding/removing rods
 
         switch (event.key) {
           case 'ArrowRight':
@@ -49,15 +53,30 @@ window.onload = function () {
             break
           case 'ArrowUp':
           case 'w':
-            event.preventDefault();
+            event.preventDefault()
             this.add(this.rods[this.activeRodIndex])
             break
           case 'ArrowDown':
           case 's':
-            event.preventDefault();
+            event.preventDefault()
             this.remove(this.rods[this.activeRodIndex])
             break
+          case '+':
+            this.addRod()
+            break
+          case 'Delete':
+          case 'Backspace':
+          case '-':
+            event.preventDefault()
+            this.removeRod(this.activeRodIndex)
+            break
         }
+      },
+      hideManual: function () {
+        this.isManualActive = false
+      },
+      showManual: function () {
+        this.isManualActive = true
       }
     }
   })
