a13f6893a84c0c0053205214c05fcd6afa8b324e
Florian Knoch <florian.knoch@stud.uni-bamberg.de>
2018-07-29 00:03:56 +0200

    Add error handling

--- js/app.js
+++ js/app.js
@@ -1,56 +1,16 @@
 window.onload = function () {
   let codeEditor;
 
-  function remove_global (index, structure) {
-    let rod
-    try {
-      rod = structure[index]
-    } catch (e) {
-      throw {
-        message: `Argument für remove() muss eine Zahl zwischen 1 und ${structure.length} sein.`
-      }
-    }
-
-    if (!isEmpty_global(index, structure)) {
-      rod.sliceCount--
-    }
-  }
-
-  function add_global (index, structure) {
-    let rod
-    try {
-      rod = structure[index]
-    } catch (e) {
-      throw {
-        message: `Argument für add() muss eine Zahl zwischen 1 und ${structure.length} sein.`
-      }
-    }
-
-    rod.sliceCount++
-  }
-
-  function isEmpty_global(index, structure) {
-    let rod
-    try {
-      rod = structure[index]
-    } catch (e) {
-      throw {
-        message: `Argument für add() muss eine Zahl zwischen 1 und ${structure.length} sein.`
-      }
-    }
-
-    return rod.sliceCount <= 0
-  }
-
   const v = new Vue({
     el: '#app',
     data: {
-      isManualActive: false,
       activeRodIndex: 0,
+      errorMessages: [],
+      isManualActive: false,
       rods: [
-        { sliceCount: 2 },
-        { sliceCount: 5 },
-        { sliceCount: 3 },
+        { sliceCount: 0 },
+        { sliceCount: 0 },
+        { sliceCount: 0 },
         { sliceCount: 0 }
       ]
     },
@@ -80,39 +40,49 @@ window.onload = function () {
       },
       keyPressed: function (event) {
         // TODO: Check for consistency throughout the OSs
-        // TODO: Add shortcut to target code area and to run code
-        if (event.target.nodeName === "TEXTAREA") return;
-
-        switch (event.key) {
-          case 'ArrowRight':
-          case 'd':
-            event.preventDefault();
-            (this.activeRodIndex < this.rods.length - 1) && this.activeRodIndex++
-            break
-          case 'ArrowLeft':
-          case 'a':
-            event.preventDefault();
-            (this.activeRodIndex > 0) && this.activeRodIndex--
-            break
-          case 'ArrowUp':
-          case 'w':
+        if (event.target.nodeName === "TEXTAREA") {
+          if (event.key == 'r' && event.ctrlKey) {
             event.preventDefault()
-            this.add(this.activeRodIndex)
-            break
-          case 'ArrowDown':
-          case 's':
-            event.preventDefault()
-            this.remove(this.activeRodIndex)
-            break
-          case '+':
-            this.addRod()
-            break
-          case 'Delete':
-          case 'Backspace':
-          case '-':
-            event.preventDefault()
-            this.removeRod(this.activeRodIndex)
-            break
+            this.runCode()
+          }
+        } else {
+          switch (event.key) {
+            case 'r':
+              if (event.ctrlKey) {
+                event.preventDefault()
+                this.runCode()
+              }
+              break
+            case 'ArrowRight':
+            case 'd':
+              event.preventDefault();
+              (this.activeRodIndex < this.rods.length - 1) && this.activeRodIndex++
+              break
+            case 'ArrowLeft':
+            case 'a':
+              event.preventDefault();
+              (this.activeRodIndex > 0) && this.activeRodIndex--
+              break
+            case 'ArrowUp':
+            case 'w':
+              event.preventDefault()
+              this.add(this.activeRodIndex)
+              break
+            case 'ArrowDown':
+            case 's':
+              event.preventDefault()
+              this.remove(this.activeRodIndex)
+              break
+            case '+':
+              this.addRod()
+              break
+            case 'Delete':
+            case 'Backspace':
+            case '-':
+              event.preventDefault()
+              this.removeRod(this.activeRodIndex)
+              break
+          }
         }
       },
       hideManual: function () {
@@ -121,74 +91,117 @@ window.onload = function () {
       showManual: function () {
         this.isManualActive = true
       },
-      runCode: async function () {
+      runCode: function () {
+        this.errorMessages = [];
+
         let code = codeEditor.getValue();
+        let context = this
+        let expressions = []
 
-        try {
-          let context = this
-          let expressions = []
+        let phantom = []
+        context.rods.forEach(function (rod) {
+          phantom.push({ sliceCount: rod.sliceCount })
+        })
 
-          let phantom = []
-          context.rods.forEach(function (rod) {
-            phantom.push({ sliceCount: rod.sliceCount })
+        let executionStartTime
+
+        const ANIMATION_TIMEOUT = 400
+        const REMOVE = 'remove'
+        const ADD = 'add'
+        const ENDLESS_LOOP_THRESHOLD = 5000
+
+        function remove (arg) {
+          if (Date.now() - executionStartTime > ENDLESS_LOOP_THRESHOLD) {
+            throw 'Der Algorithmus benötigt sehr viel Zeit und wurde gestoppt. Eventuell liegt eine Endlosschleife vor.'
+          }
+
+          remove_global(arg - 1, phantom)
+
+          expressions.push({
+            action: REMOVE,
+            index: arg - 1
           })
+        }
 
-          const TIMEOUT = 400
-          const REMOVE = 'remove'
-          const ADD = 'add'
-
-          function remove (arg) {
-            remove_global(arg - 1, phantom)
-
-            expressions.push({
-              action: REMOVE,
-              index: arg - 1
-            })
+        function add (arg) {
+          if (Date.now() - executionStartTime > ENDLESS_LOOP_THRESHOLD) {
+            throw 'Der Algorithmus benötigt sehr viel Zeit und wurde gestoppt. Eventuell liegt eine Endlosschleife vor.'
           }
 
-          function add (arg) {
-            add_global(arg - 1, phantom)
+          add_global(arg - 1, phantom)
 
-            expressions.push({
-              action: ADD,
-              index: arg - 1
-            })
-          }
+          expressions.push({
+            action: ADD,
+            index: arg - 1
+          })
+        }
 
-          function isEmpty (arg) {
-            return isEmpty_global(arg - 1, phantom)
-          }
+        function isEmpty (arg) {
+          return isEmpty_global(arg - 1, phantom)
+        }
 
-          code = code.replace(/((NOT)|(not)|(Not))[\t\n]*/, '!')
-          eval(code);
-
-          // TODO: Implement max run time
-          // TODO: Enable viewer to change playback speed
-          // TODO: Add language documentation
-          // TODO: Make algorithm panel invisible
-
-          (function parseNextExpression (expressions) {
-            if (expressions.length == 0) return;
-            expression = expressions.shift()
-
-            switch (expression.action) {
-              case REMOVE:
-                context.remove(expression.index)
-                break
-              case ADD:
-                context.add(expression.index)
-            }
-
-            setTimeout(() => parseNextExpression(expressions), TIMEOUT)
-          })(expressions)
-
+        code = code.replace(/((NOT)|(not)|(Not))[\t\n]*/, '!')
+        try {
+          executionStartTime = Date.now()
+          eval(code)
         } catch (e) {
-          // TODO: Output
-          console.log(e.message)
+          this.errorMessages = []
+          this.errorMessages.push(e.message || e)
+        }
+
+        // TODO: Make algorithm panel invisible
+
+        function parseNextExpression (expressions) {
+          if (expressions.length == 0) return;
+          expression = expressions.shift()
+
+          switch (expression.action) {
+            case REMOVE:
+              context.remove(expression.index)
+              break
+            case ADD:
+              context.add(expression.index)
+          }
+
+          setTimeout(() => parseNextExpression(expressions), ANIMATION_TIMEOUT)
+        }
+
+        if (this.errorMessages.length == 0) {
+          parseNextExpression(expressions)
         }
       }
     }
   })
+
+  function remove_global(index, structure) {
+    if (!(typeof index === "number") || index >= structure.length || index < 0) {
+      throw `Argument für <code>remove()</code> muss eine Zahl zwischen 1 und ${structure.length} sein.`
+    } else {
+      let rod = structure[index]
+
+      if (!isEmpty_global(index, structure)) {
+        rod.sliceCount--
+      }
+    }
+  }
+
+  function add_global(index, structure) {
+    if (!(typeof index === "number") || index >= structure.length || index < 0) {
+      throw `Argument für <code>add()</code> muss eine Zahl zwischen 1 und ${structure.length} sein.`
+    } else {
+      let rod = structure[index]
+      rod.sliceCount++
+    }
+  }
+
+  function isEmpty_global(index, structure) {
+    if (!(typeof index === "number") || index >= structure.length || index < 0) {
+      throw `Argument für isEmpty() muss eine Zahl zwischen 1 und ${structure.length} sein.`
+    } else {
+      let rod = structure[index]
+      return rod.sliceCount <= 0
+    }
+  }
 
   codeEditor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     mode: 'javascript',
